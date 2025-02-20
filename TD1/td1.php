7abb347b551a914c6dc56ee5de9010e1e33a000b
Kez0X <118253031+Kez0X@users.noreply.github.com>
2025-02-20 07:25:20 +0000

    feat : TD1

---
+++ TD1/td1.php
@@ -0,0 +1,47 @@
+<?php 
+    require("tbs_class.php");
+    $tbs = new clsTinyButStrong;
+
+    class HelloWorld{
+        private $n;
+
+        function __construct($n){
+            $this->n = $n;
+
+        }
+
+        public function retournerN(){
+            return $this->n;
+        }
+    }
+    $hw = new HelloWorld(2);
+    $valeur = $hw->retournerN();
+
+    class Tableau{
+        private $t;
+
+        function __construct($param_ta){
+            $this->t = $param_ta;
+        }
+
+        public function retourne(){
+            return $this->t;
+        }
+    }
+
+    class Matrice extends Tableau{
+
+        public function somme(){
+            for ()
+        }
+    }
+
+    $tab = new Tableau(array("Valeriia" => 16, "Vadim" => 12));
+    $chaine = $tab->retourne();
+
+    $tbs->LoadTemplate("gab1.tpl.td1.html");
+    $tbs->MergeBlock("chaine",$chaine);
+    $tbs->Show(); 
+
+?>
+
